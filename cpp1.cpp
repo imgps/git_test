616312a3684b1a9b321ca8f5078d41eef17f6bed
guru27 <guru444ps@gmail.com>
2022-08-15 14:38:12 +0530

    Adding commit cpp

---
+++ cpp1.cpp
@@ -0,0 +1,16 @@
+// sample cpp program to multiply 2 numbers
+// Practice
+
+#include<iostream>
+using namespace std;
+int main() {
+
+    int a, b, sum;
+    cout<<"Enter the first number: \n";
+    cin>> a;
+    cout<<"Enter the second number \n";
+    cin>> b;
+    sum = a*b;
+    cout<<"The product is " << sum;
+    return 0;
+}
