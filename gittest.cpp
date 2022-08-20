4b685b2cc0800ae3eb38b296f05b17da6842d3f4
guru27 <guru444ps@gmail.com>
2022-08-20 13:05:53 +0530

    Testing gittest

---
+++ gittest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int i,j,sum;
+    cout<< "Enter the first number: " << endl;
+    cin>> i;
+
+    cout<<"Enter the second number: " << endl;
+    cin>> j;
+
+    sum = i+j;
+    cout<< "The sum of the two digits are: " << sum << endl;
+    return 0;
+}
