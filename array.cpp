b0bc01a0abed54cc7257449f01f8e62a679990c2
guru27 <guru444ps@gmail.com>
2022-08-22 14:39:11 +0530

    Git and bash cpp/nano editor

--- array.cpp
+++ array.cpp
@@ -1,10 +1,13 @@
+//Edited in nano, using gitbash//
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
     
     int n;
-    cout<<"Enter size";
+    cout<<"Enter size: ";
     cin>>n;
     int A[n];
     A[0]=2;
---
+++ nano2.cpp
@@ -0,0 +1,21 @@
+/*this is another sample cpp program
+writted in nano text editor using git bash and building and saving it and commit 
+to the git*/
+
+#include<iostream>
+using namespace std;
+int main() {
+
+	int a;
+	cout<<"Enter the digit: "<< endl;
+	cin>>a;
+
+	if(a>10)
+	{	
+		cout << a <<"  = is greateer than 10" << endl;
+	}else
+		{	
+			cout<< a <<"  = is lesser than 10" << endl;
+		}
+	return 0;
+}
