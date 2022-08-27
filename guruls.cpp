1d18a7661c2789860f8d591bfbd90d379a0adae8
guru27 <guru444ps@gmail.com>
2022-08-27 13:47:42 +0530

    Commit

---
+++ guruls.cpp
@@ -0,0 +1,15 @@
+/* this is a practice program */
+
+#include<iostream>
+using namespace std;
+int main () {
+
+	int a;
+	cout<<"Enter the number: "<< endl;
+	cin >>a;
+	if(a % 2 == 0)
+		cout<< a << "   Is even.."<<endl;
+	else
+		cout<<a<<"  Is ODDDDDDD"<<endl;
+	return 0;
+}
